5c6cea879bdb4c7d213864e34b43e6521c007802
Mohammad Pesaraklou <mhmdpsr6@gmail.com>
2022-12-12 20:31:19 +0330

    create the structure and start the project

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,12 @@
+
+export default function Trivia({}) {
+
+  return (
+    <div className="trivia">
+      <div className="question"></div>
+      <div className="answers">
+
+      </div>
+    </div>
+  );
+}
